0bb26c5ce570705e849e0e922e75d95386a3ec0d
ShinKRS <124538420+ShinKRS@users.noreply.github.com>
2025-04-03 22:55:26 -0500

    Update Ops.java

--- Corte 1/GUI/Ops.java
+++ Corte 1/GUI/Ops.java
@@ -1,1 +1,20 @@
+package calculadoraops;
 
+public class Ops {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int potencia(int base, int exponente) {
+        return exponente == 0 ? 1 : base * potencia(base, exponente - 1);
+    }
+}
