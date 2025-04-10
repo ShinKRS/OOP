b4c4db7db7e95db8cbd1da637a8455d5ebe812e1
JuanOsorio <124538420+ShinKRS@users.noreply.github.com>
2025-04-10 17:44:37 -0500

    Update Main.java

--- Corte 1/Tareas Corte 1/Main.java
+++ Corte 1/Tareas Corte 1/Main.java
@@ -1,16 +1,17 @@
 public class Main {
-
-    
     public static void main(String[] args) {
-     Operations resultado = new Operations();
-     //suma
-     resultado.sumar(3,2);
-     System.out.println("La suma es: "+resultado.r);
- //resta
-     resultado.restar(9,6);
-     System.out.println("La resta es: "+resultado.r);
-     //multiplicacion
-     resultado.multiplicar(2,5);
-     System.out.println("la multiplicacion es: "+resultado.r);
+        Operations resultado = new Operations();
+
+        resultado.setSuma(3, 2);
+        System.out.println("Suma: " + resultado.getResultado());
+
+        resultado.setResta(9, 6);
+        System.out.println("Resta: " + resultado.getResultado());
+
+        resultado.setMultiplicacion(2, 5);
+        System.out.println("Multiplicación: " + resultado.getResultado());
+
+        resultado.setPotencia(2, 3);
+        System.out.println("Potencia: " + resultado.getResultado());
     }
 }
