f03d3544ec9c7807e07845aee37df01b5d1da495
JuanOsorio <124538420+ShinKRS@users.noreply.github.com>
2025-04-24 17:52:04 -0500

    Add files via upload

---
+++ Corte 1/Tarea3/Operations.java
@@ -0,0 +1,40 @@
+package operations;
+
+import javax.swing.JOptionPane;
+public class Operations {
+    private int result;
+
+    // Getter del resultado
+    public String getResultado(int numero1, int numero2) {
+        String mensaje = "Result = " + result;
+        JOptionPane.showMessageDialog(null, mensaje);
+        return mensaje;
+    }
+
+    // Setters personalizados para operaciones
+    public int setSuma(int a, int b) {
+        result = a+b;
+        return result;
+    }
+
+    public void setResta(int a, int b) {
+        result = a - b;
+        
+    }
+
+    public void setMultiplicacion(int a, int b) {
+        result = a * b;
+
+    }
+
+    public void setPotencia(int base, int exponente) {
+        result = calcularPotencia(base, exponente);
+        
+    }
+
+    // Método auxiliar para la potencia
+    private int calcularPotencia(int base, int exponente) {
+        result = exponente == 0 ? 1 : base * calcularPotencia(base, exponente - 1);
+        return result;
+    }
+}
