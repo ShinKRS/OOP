000f0fb7f9d28ad9c491ab0e55785da16b0f61ba
JuanOsorio <124538420+ShinKRS@users.noreply.github.com>
2025-07-02 21:38:59 -0500

    Update Arcoiris.cpp

--- Corte2/Arduino (.h)/Arcoiris.cpp
+++ Corte2/Arduino (.h)/Arcoiris.cpp
@@ -1,40 +1,61 @@
 #include "Arcoiris.h"
 #include <Arduino.h>
 
-Arcoiris::Arcoiris(Led* a, Led* r, Led* v, int tiempo) {
+Arcoiris::Arcoiris(Led* a, Led* r, Led* v, Led* z, int tiempo) {
   this->amarillo = a;
   this->rojo = r;
   this->verde = v;
+  this->azul = z;
   this->tiempo = tiempo;
 }
 
 void Arcoiris::mostrarSecuencia() {
-  // Solo uno encendido a la vez
-  amarillo->encender(); rojo->apagar(); verde->apagar();
+  // Encender uno a la vez
+  amarillo->encender(); rojo->apagar(); verde->apagar(); azul->apagar();
   delay(tiempo);
 
-  amarillo->apagar(); rojo->encender(); verde->apagar();
+  amarillo->apagar(); rojo->encender(); verde->apagar(); azul->apagar();
   delay(tiempo);
 
-  amarillo->apagar(); rojo->apagar(); verde->encender();
+  amarillo->apagar(); rojo->apagar(); verde->encender(); azul->apagar();
   delay(tiempo);
 
-  // Combinaciones
-  amarillo->encender(); rojo->encender(); verde->apagar();
+  amarillo->apagar(); rojo->apagar(); verde->apagar(); azul->encender();
   delay(tiempo);
 
-  amarillo->apagar(); rojo->encender(); verde->encender();
+  // Encender combinaciones de dos
+  amarillo->encender(); rojo->encender(); verde->apagar(); azul->apagar();
   delay(tiempo);
 
-  amarillo->encender(); rojo->apagar(); verde->encender();
+  amarillo->apagar(); rojo->encender(); verde->encender(); azul->apagar();
+  delay(tiempo);
+
+  amarillo->apagar(); rojo->apagar(); verde->encender(); azul->encender();
+  delay(tiempo);
+
+  amarillo->encender(); rojo->apagar(); verde->apagar(); azul->encender();
+  delay(tiempo);
+
+  // Encender tres
+  amarillo->encender(); rojo->encender(); verde->encender(); azul->apagar();
+  delay(tiempo);
+
+  rojo->encender(); verde->encender(); azul->encender(); amarillo->apagar();
+  delay(tiempo);
+
+  amarillo->encender(); verde->encender(); azul->encender(); rojo->apagar();
+  delay(tiempo);
+
+  amarillo->encender(); rojo->encender(); azul->encender(); verde->apagar();
   delay(tiempo);
 
   // Todos encendidos
-  amarillo->encender(); rojo->encender(); verde->encender();
+  amarillo->encender(); rojo->encender(); verde->encender(); azul->encender();
   delay(tiempo);
 
   // Todos apagados
-  amarillo->apagar(); rojo->apagar(); verde->apagar();
+  amarillo->apagar(); rojo->apagar(); verde->apagar(); azul->apagar();
   delay(tiempo);
 }
+
 
